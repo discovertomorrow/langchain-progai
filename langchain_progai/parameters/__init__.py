11337f2533573aab38ea7ae43695565c0a7f8cac
mdoerr <manuel.doerr@prognostica.de>
2024-05-27 15:24:56 +0200

    remove outdated code and remove default config

--- langchain_progai/parameters/__init__.py
+++
@@ -1,3 +0,0 @@
-from .model_parameters import Zephyr, OpenChat, Mistral7bInstruct
-
-__all__ = ["Zephyr", "OpenChat", "Mistral7bInstruct"]
--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -1,4 +1,6 @@
+import pytest
 import os
+import shutil
 import logging
 import langchain_progai.config as config
 from importlib import resources
@@ -7,13 +9,31 @@ from importlib import resources
 logger = logging.getLogger(__name__)
 
 
+@pytest.fixture
+def setup_test_config():
+    """Fixture to make sure that at least a template config.yaml is available during test execution."""
+    config_path = str(resources.files("langchain_progai") / "config/config.yaml")
+    template_path = str(resources.files("langchain_progai") / "config/config.yaml.template")
+    file_was_copied = False
+
+    if not os.path.exists(config_path):
+        shutil.copy(template_path, config_path)
+        file_was_copied = True
+
+    yield ...
+
+    # Clean up: delete config.yaml if it was copied
+    if file_was_copied and os.path.exists(config_path):
+        os.remove(config_path)
+
+
 def _assert_valid_config(conf: dict):
     assert isinstance(conf, dict)
     assert "endpoints" in conf
     assert len(conf["endpoints"]) > 0
 
 
-def test_load_config___given_no_input___loads_default_config():
+def test_load_config___given_no_input___loads_default_config(setup_test_config):
     # Arrange & Act
     conf = config.load_config(path=None)
 
@@ -21,7 +41,7 @@ def test_load_config___given_no_input___loads_default_config():
     _assert_valid_config(conf)
 
 
-def test_load_config___given_wrong_path___returns_fallback(caplog):
+def test_load_config___given_wrong_path___returns_fallback(caplog, setup_test_config):
     # Arrange & Act
     conf = config.load_config(path="NonExistingPath.yaml")
 
@@ -30,9 +50,9 @@ def test_load_config___given_wrong_path___returns_fallback(caplog):
     assert "No configuration file found" in caplog.text
 
 
-def test_load_config___given_path_in_environment_variable___works_as_expected(caplog):
+def test_load_config___given_path_in_environment_variable___works_as_expected(caplog, setup_test_config):
     # Arrange
-    os.environ['LANGCHAIN_PROGAI_CONFIG'] = str(resources.files("langchain_progai") / "config/default_config.yaml")
+    os.environ['LANGCHAIN_PROGAI_CONFIG'] = str(resources.files("langchain_progai") / "config/config.yaml")
 
     # Act
     conf = config.load_config(path=None)
@@ -42,16 +62,15 @@ def test_load_config___given_path_in_environment_variable___works_as_expected(ca
     assert "No configuration file found" not in caplog.text
 
 
-def test_get_endpoint___given_endpoint_name___returns_endpoint_string():
+def test_get_endpoint___given_endpoint_name___returns_endpoint_string(setup_test_config):
     # Arrange & Act
     endpoint = config.get_endpoint("DOLPHIN")
 
     # Assert
     assert isinstance(endpoint, str)
-    assert endpoint.startswith("http")
 
 
-def test_get_endpoint___endpoint_in_env___overwrites_default_configuration():
+def test_get_endpoint___endpoint_in_env___overwrites_default_configuration(setup_test_config):
     # Arrange
     name = "DOLPHIN"
     test_endpoint = "http://test"
