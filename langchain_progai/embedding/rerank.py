ccf5e06ae5c0db60260d354ffbf2af7cffafd39b
Arne Müller <arne.mueller@prognostica.de>
2024-05-31 16:52:16 +0200

    ensure that the ranking is based on the score

--- langchain_progai/embedding/rerank.py
+++ langchain_progai/embedding/rerank.py
@@ -1,4 +1,3 @@
-import heapq
 import requests
 import os
 import asyncio
@@ -23,7 +22,8 @@ class RerankCompressor(BaseDocumentCompressor):
     @root_validator(allow_reuse=True)
     def validate_endpoint(cls, values: Dict) -> Dict:
         """If no endpoint is set explicitly, try to fallback to configuration file or environment variables."""
-        values["endpoint"] = values["endpoint"] or get_endpoint("RERANKER_BGE_L")
+        values["endpoint"] = values["endpoint"] or get_endpoint(
+            "RERANKER_BGE_L")
         return values
 
     def _generate_headers(self):
@@ -37,11 +37,12 @@ class RerankCompressor(BaseDocumentCompressor):
         documents: Sequence[Document],
         query: str,
     ):
-        data = {"query": query, "texts": [document.page_content for document in documents]}
-        response = requests.post(self.endpoint, headers=self._generate_headers(), json=data)
+        data = {"query": query, "texts": [
+            document.page_content for document in documents]}
+        response = requests.post(
+            self.endpoint, headers=self._generate_headers(), json=data)
 
         if response.status_code == 200:
-            print(response.json())
             return [(s["index"], s["score"]) for s in response.json()]
         else:
             print("Error:", response.status_code, response.text)
@@ -54,9 +55,10 @@ class RerankCompressor(BaseDocumentCompressor):
         callbacks: Optional[Callbacks] = None,
     ) -> Sequence[Document]:
         scores = self._get_scores_from_endpoint(documents, query)
-        result = [(score, documents[index]) for index, score in scores]
-        ranked = heapq.nlargest(self.top_k, result)
-        return [doc for _, doc in ranked]
+        top_k_scores = sorted(scores, key=lambda x: x[1], reverse=True)[
+            :self.top_k]
+        result = [(score, documents[index]) for index, score in top_k_scores]
+        return [doc for _, doc in result]
 
     async def acompress_documents(
         self,
